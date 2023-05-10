44c8a85d694b4446407df36fc1b33621f4007deb
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-10 14:25:54 -0300

    making footer responsive

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter,BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Shop from "./pages/Shop";
--- src/components/header.js
+++ src/components/header.js
@@ -14,11 +14,34 @@ function Header() {
     <div className="headerClass">
        <div className="leftSpace">
         <div className='Logo'>Logo</div>
-       </div> 
+       </div>        
        <div className="rightSpace">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/shop">Shop</a>
+        <button className="hamburger">
+          {/* icon from heroicons.com */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+
+        <div
+        className="navigation-menu">
+          <ul>
+            
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/shop">Shop</a></li>
+          
+          </ul>
+        </div>  
         <p>Cart</p>  
        </div> 
       
