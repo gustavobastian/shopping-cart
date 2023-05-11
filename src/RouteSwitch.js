150a53a9e11ae00de2c64fa492d75360287659be
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-05-10 22:59:14 -0300

    adding items to chart

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -5,18 +5,31 @@ import About from "./pages/About";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 
+import { useState } from "react";
 
 
 const RouteSwitch = () => {
 
+  const [buyList,setBuyList]=useState({list:[{"id":0,"name":"","price":""}]});
+
+  const addElement=(value)=>{
+    console.log("on top:"+JSON.stringify(value));    
+    let myString = JSON.stringify(value);
+    
+    let myObject=JSON.parse(myString);
+    
+    
+    setBuyList({list:buyList.list.concat(value)});
+    return 1;
+  }
 
   return (
     <BrowserRouter>      
       <Routes>        
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/" element={<Home value={buyList}/>} />
+        <Route path="/about" element={<About value={buyList} />} />
+        <Route path="/shop" element={<Shop value={buyList} addElement={(event)=>{addElement(event)}} />} />
+        <Route path="/cart" element={<Cart value={buyList}/>} />
          {/* 👇️ only match this when no other routes match */}
          <Route
             path="*"
